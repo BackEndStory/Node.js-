15ee968c023e5f45f7b6cb1f9ffafea49ee1091b
seungryeol <opop0421@naver.com>
2023-08-11 23:20:17 +0900

    메모리 누수가 일어나는 이유 (return)

--- ch5/무한 재귀 프로미스/index.js
+++ ch5/무한 재귀 프로미스/index.js
@@ -24,6 +24,10 @@ function nonLeakingLoop() {
 }
 
 
+
+
+
+
 function nonLeakingLoopWithErrors() {
     return new Promise((resolve, reject) => {
         (function internalLoop() {
@@ -54,6 +58,15 @@ async function nonLeakingLoopAsync () {
   }
 
 
-//leakingLoop()
+//   leakingLoopAsync 함수는 재귀적으로 자신을 호출하고 있습니다. 이 재귀 호출은 await delay(1)로 인해 프라미스의 해결을 기다린 후에 수행됩니다. 그러나 재귀 호출을 반환할 때 기존 함수 스코프가 스택에 쌓이게 되므로, 매 호출마다 새로운 스택 프레임이 생성되고 결국 메모리가 누수될 가능성이 있습니다.
 
-nonLeakingLoop()
+// 반면에 nonLeakingLoopAsync 함수는 무한 루프 내에서 await delay(1)을 사용하고 있습니다. 이 경우 await 키워드로 프라미스의 해결을 기다리면서도 현재 함수 스코프는 중단되고 이벤트 루프로 돌아갑니다. 따라서 새로운 루프 반복이 시작될 때 이전 반복의 스택 프레임은 이미 해제되어있고 메모리 누수가 발생하지 않습니다.
+
+
+for (let i = 0; i < 1e6; i++) {
+    leakingLoop()
+    nonLeakingLoop()
+    nonLeakingLoopWithErrors()
+    nonLeakingLoopAsync()
+    leakingLoopAsync()
+  }
