8254903a2bb600189c3cc6dfc2fa4ec5859c2ea5
seungryeol <opop0421@naver.com>
2023-08-20 22:19:59 +0900

    Writable  스트림

---
+++ ch6/Writable 스트림/entropy-server.mjs
@@ -0,0 +1,39 @@
+import { createServer } from 'http'
+import Chance from 'chance'
+
+const chance = new Chance()
+
+// const server = createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' }) // ①
+//   while (chance.bool({ likelihood: 95 })) { // ②
+//     res.write(`${chance.string()}\n`) // ③
+//   }
+//   res.end('\n\n') // ④
+//   res.on('finish', () => console.log('All data sent')) // ⑤
+// })
+
+// server.listen(8080, () => {
+//   console.log('listening on http://localhost:8080')
+// })
+
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    function generateMore () { // ①
+      while (chance.bool({ likelihood: 95 })) {
+        const randomChunk = chance.string({ length: (16 * 1024) - 1 }) // ②
+        const shouldContinue = res.write(`${randomChunk}\n`) // ③
+        if (!shouldContinue) {
+          console.log('back-pressure')
+          return res.once('drain', generateMore)
+        }
+      }
+      res.end('\n\n')
+    }
+    generateMore()
+    res.on('finish', () => console.log('All data sent'))
+  })
+  
+  server.listen(8080, () => {
+    console.log('listening on http://localhost:8080')
+  })
---
+++ ch6/Writable 스트림/to-file-stream.mjs
@@ -0,0 +1,33 @@
+import { Writable } from 'stream'
+import { promises as fs } from 'fs'
+import { dirname, join } from 'path'
+import mkdirp from 'mkdirp-promise'
+
+export class ToFileStream extends Writable {
+  constructor (options) {
+    super({ ...options, objectMode: true })
+  }
+
+  _write (chunk, encoding, cb) {
+    mkdirp(dirname(chunk.path))
+      .then(() => fs.writeFile(chunk.path, chunk.content))
+      .then(() => cb())
+      .catch(cb)
+  }
+}
+
+
+const tfs = new Writable({
+    objectMode: true,
+    write (chunk, encoding, cb) {
+      mkdirp(dirname(chunk.path))
+        .then(() => fs.writeFile(chunk.path, chunk.content))
+        .then(() => cb())
+        .catch(cb)
+    }
+  })
+  
+  tfs.write({ path: join('files', 'file1.txt'), content: 'Hello' })
+  tfs.write({ path: join('files', 'file2.txt'), content: 'Node.js' })
+  tfs.write({ path: join('files', 'file3.txt'), content: 'streams' })
+  tfs.end(() => console.log('All files created'))
